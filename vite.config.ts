35b2ddfeee0948c7cbe9c0b0771d1cf24337aafd
Avner Hattab <avner.h@nanox.vision>
2024-07-10 14:37:09 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = process.env.PORT ?  +process.env.PORT : 3000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: port,
+    host: '0.0.0.0'
+  }
+});
